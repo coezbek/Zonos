c0f4b054ece59c68c14ff2347f989b4a4b016c8f
Christopher Oezbek <c.oezbek@gmail.com>
2025-03-20 13:34:49 +0100

    Use left-padding rather than right-padding for prefix audio

--- zonos/autoencoder.py
+++ zonos/autoencoder.py
@@ -15,9 +15,10 @@ class DACAutoencoder:
         self.sampling_rate = self.dac.config.sampling_rate
 
     def preprocess(self, wav: torch.Tensor, sr: int) -> torch.Tensor:
+        """Resample and left-pad the input waveform for decoding with the DACAutoencoder."""
         wav = torchaudio.functional.resample(wav, sr, 44_100)
-        right_pad = math.ceil(wav.shape[-1] / 512) * 512 - wav.shape[-1]
-        return torch.nn.functional.pad(wav, (0, right_pad))
+        left_pad = math.ceil(wav.shape[-1] / 512) * 512 - wav.shape[-1]
+        return torch.nn.functional.pad(wav, (left_pad, 0), value=0)
 
     def encode(self, wav: torch.Tensor) -> torch.Tensor:
         return self.dac.encode(wav).audio_codes
